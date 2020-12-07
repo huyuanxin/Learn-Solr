6a38d320d4036c6118bde7d3d88490718b91b082
huyuanxin <huyuanxin1999@outlook.com>
2020-12-07 16:31:56 +0800

    init

---
+++ src/test/java/yuanxin/learn/solr/SolrApplicationTests.java
@@ -0,0 +1,13 @@
+package yuanxin.learn.solr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

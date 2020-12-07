547ee123c95e5963abc41f735d80c15c3bb92f47
huyuanxin <huyuanxin1999@outlook.com>
2020-12-07 18:05:11 +0800

    完善转换逻辑

--- src/main/java/yuanxin/learn/solr/util/ModelToSolrDocument.java
+++ src/main/java/yuanxin/learn/solr/util/ModelToSolrDocument.java
@@ -1,6 +1,7 @@
 package yuanxin.learn.solr.util;
 
 import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.StringUtils;
 import yuanxin.learn.solr.po.Demo;
 
 /**
@@ -11,10 +12,20 @@ public class ModelToSolrDocument {
     public static SolrInputDocument DemoToSolrDocument(Demo demo) {
         // Demo转为SolrInputDocument
         SolrInputDocument document = new SolrInputDocument();
-        document.addField("id", demo.getUid());
-        document.addField("Name", demo.getName());
-        document.addField("Desc", demo.getDesc());
-        document.addField("create_time", demo.getDate());
+        if (!StringUtils.isEmpty(demo.getUid())) {
+            document.addField("id", demo.getUid());
+        }
+        if (!StringUtils.isEmpty(demo.getName())) {
+            document.addField("Name", demo.getName());
+
+        }
+        if (!StringUtils.isEmpty(demo.getDesc())) {
+            document.addField("Desc", demo.getDesc());
+
+        }
+        if (demo.getName() != null) {
+            document.addField("create_time", demo.getDate());
+        }
         return document;
     }
 }

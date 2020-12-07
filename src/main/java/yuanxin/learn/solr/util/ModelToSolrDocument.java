23c5a1fb6b0df9a85fcf54599b839011f96b07c8
huyuanxin <huyuanxin1999@outlook.com>
2020-12-07 18:16:27 +0800

    添加一些注释

--- src/main/java/yuanxin/learn/solr/util/ModelToSolrDocument.java
+++ src/main/java/yuanxin/learn/solr/util/ModelToSolrDocument.java
@@ -9,19 +9,23 @@ import yuanxin.learn.solr.po.Demo;
  * @create 2020/12/7 15:30
  */
 public class ModelToSolrDocument {
+    /**
+     * Demo转为SolrInputDocument
+     *
+     * @param demo 传入的demo
+     * @return 返回demo转换的document
+     */
     public static SolrInputDocument demoToSolrDocument(Demo demo) {
-        // Demo转为SolrInputDocument
         SolrInputDocument document = new SolrInputDocument();
+        // 判断是不是空
         if (!StringUtils.isEmpty(demo.getUid())) {
             document.addField("id", demo.getUid());
         }
         if (!StringUtils.isEmpty(demo.getName())) {
             document.addField("Name", demo.getName());
-
         }
         if (!StringUtils.isEmpty(demo.getDesc())) {
             document.addField("Desc", demo.getDesc());
-
         }
         if (demo.getName() != null) {
             document.addField("create_time", demo.getDate());

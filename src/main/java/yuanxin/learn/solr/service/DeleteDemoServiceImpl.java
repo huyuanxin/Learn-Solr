86db64f97ff5c18d0225e119c60b71e3b08164e9
huyuanxin <huyuanxin1999@outlook.com>
2020-12-07 16:28:37 +0800

    将Demo从Solr删除的接口的实现

---
+++ src/main/java/yuanxin/learn/solr/service/DeleteDemoServiceImpl.java
@@ -0,0 +1,41 @@
+package yuanxin.learn.solr.service;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import yuanxin.learn.solr.api.DeleteDemoService;
+
+import java.io.IOException;
+
+/**
+ * @author huyuanxin
+ * @create 2020/12/7 16:15
+ */
+@Service("DeleteDemoService")
+public class DeleteDemoServiceImpl implements DeleteDemoService {
+    private SolrClient solrClient;
+
+    @Autowired
+    public DeleteDemoServiceImpl(SolrClient solrClient) {
+        this.solrClient = solrClient;
+    }
+
+    /**
+     * 删除Solr的单个Demo
+     *
+     * @param uid Demo的Uid
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteDemo(String uid) {
+        try {
+            solrClient.deleteById(uid);
+            solrClient.commit();
+            return true;
+        } catch (IOException | SolrServerException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}

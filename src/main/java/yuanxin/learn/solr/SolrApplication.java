a10816a6bb541b700cbe2f67b592f6aef99bab54
huyuanxin <huyuanxin1999@outlook.com>
2020-12-07 16:30:59 +0800

    启动

---
+++ src/main/java/yuanxin/learn/solr/SolrApplication.java
@@ -0,0 +1,16 @@
+package yuanxin.learn.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huyuanxin
+ */
+@SpringBootApplication
+public class SolrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolrApplication.class, args);
+    }
+
+}

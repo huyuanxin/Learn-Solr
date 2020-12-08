bf69c6e314a98c3126e115f55571e60cc0ba04d7
huyuanxin <huyuanxin1999@outlook.com>
2020-12-08 17:15:41 +0800

    Swagger3配置

---
+++ src/main/java/yuanxin/learn/solr/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package yuanxin.learn.solr.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author huyuanxin
+ * @create 2020/12/8 16:11
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger3接口文档")
+                .description("yuanxin的Solr学习项目")
+                .contact(new Contact("yuanxin", "http://github.com/huyuanxin", "huyuanxin1999@outlook.com"))
+                .version("1.0")
+                .build();
+    }
+}

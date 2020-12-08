23e512e9856128542312eb5f0a792e218ca5c086
huyuanxin <huyuanxin1999@outlook.com>
2020-12-08 10:21:40 +0800

    添加通过年龄范围查询Solr内Demo的接口

--- src/main/java/yuanxin/learn/solr/api/SearchDemoService.java
+++ src/main/java/yuanxin/learn/solr/api/SearchDemoService.java
@@ -12,10 +12,18 @@ import java.util.List;
 @Service("SearchDemoService")
 public interface SearchDemoService {
     /**
-     * 查询所以demo
+     * 查询所有demo
      *
      * @return 查询结果
      */
     List<Demo> searchAllDemo();
 
+    /**
+     * 通过年龄范围查询Solr内的Demo
+     *
+     * @param start  起始
+     * @param ending 结尾
+     * @return 查询结果 {@link List<Demo>}
+     */
+    List<Demo> searchDemoWithAgeFitter(int start, int ending);
 }
